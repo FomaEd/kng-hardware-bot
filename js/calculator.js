fef3a4cca54ff9366373896355557ceb9784abe6
FomaEd <164784372+FomaEd@users.noreply.github.com>
2026-04-06 13:53:57 +0300

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,1 +1,6 @@
-//Пустой
+function pickVisibleTurnTiltSet(height, width) {
+        return hardwareMatrixVisibleTurnTilt.find(row =>
+            height >= row.minHeight && height <= row.maxHeight &&
+            width >= row.minWidth && width <= row.maxWidth
+        );
+    }
